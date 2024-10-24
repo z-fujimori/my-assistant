c6f487c4f13990a65ff1e9f22a5a690c6ab657ed
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2024-10-24 14:36:21 +0900

    number increment

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-import Keyboard from "./components/Keyboard";
+import Keyboard from "./features/calculator/components/element/Keyboard";
+import CalcuCard from "./features/calculator/components/CalcuCard";
 
 function App() {
-  const [number, setNumber] = useState(0);
-
-  const updateNumber = ( x:number ): void => setNumber(x);
 
   async function greet() {
 
   }
 
   return (
-    <main className="container">
       <div>
         <h1> Welcom </h1>
-        <p> {number} </p>
-      </div>
 
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex">
-          <Keyboard value={1} updateNumber={updateNumber(1)} />
-          <Keyboard value={2} updateNumber={updateNumber(2)} />
-          <Keyboard value={3} updateNumber={updateNumber(3)} />
-        </div>
-        <div className="flex">
-          <Keyboard value={4} updateNumber={updateNumber(4)} />
-          <Keyboard value={5} updateNumber={updateNumber(5)} />
-          <Keyboard value={6} updateNumber={updateNumber(6)} />
-        </div>
-        <div className="flex">
-          <Keyboard value={7} updateNumber={updateNumber(7)} />
-          <Keyboard value={8} updateNumber={updateNumber(8)} />
-          <Keyboard value={9} updateNumber={updateNumber(9)} />
-        </div>
-        <div>
-          <Keyboard value={0} updateNumber={updateNumber(0)} />
-        </div>
+        <CalcuCard />
       </div>
-
-    </main>
   );
 }
 
---
+++ src/features/calculator/components/CalcuCard.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Keyboard from './element/Keyboard'
+
+const CalcuCard = () => {
+
+    const [number, setNumber] = useState(0);
+    function updateNumber (x:number) {
+        setNumber(number*10 + x);
+    }
+
+    
+    return (
+        <div>
+            <div className="flex flex-col items-center justify-center">
+                <p> {number} </p>
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
+                <div className="flex">
+                    <Keyboard value={1} updateNumber={updateNumber} />
+                    <Keyboard value={2} updateNumber={updateNumber} />
+                    <Keyboard value={3} updateNumber={updateNumber} />
+                </div>
+                <div className="flex">
+                    <Keyboard value={4} updateNumber={updateNumber} />
+                    <Keyboard value={5} updateNumber={updateNumber} />
+                    <Keyboard value={6} updateNumber={updateNumber} />
+                </div>
+                <div className="flex">
+                    <Keyboard value={7} updateNumber={updateNumber} />
+                    <Keyboard value={8} updateNumber={updateNumber} />
+                    <Keyboard value={9} updateNumber={updateNumber} />
+                </div>
+                <div>
+                    <Keyboard value={0} updateNumber={updateNumber} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CalcuCard
