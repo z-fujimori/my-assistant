33037e9f702b46222af126551c89a2cda1e7936e
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2024-12-21 20:43:27 +0900

    遅れてモーダル表示(たくさん出るのを防止するため)

--- src/features/task/components/Stamp.tsx
+++ src/features/task/components/Stamp.tsx
@@ -14,30 +14,44 @@ const Stamp = (props:{
   const timeValue = (props.dayleTime?.start_date ? props.dayleTime.time : 0);
   const [isHovered, setIsHovered] = useState(false);
   const timeoutRef = useRef<number | null>(null);
+  const timerRef = useRef<number | null>(null);
   const containerRef = useRef<HTMLDivElement>(null);
 
   const handleMouseEnter = () => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-    setIsHovered(true);
+    // if (timeoutRef.current) {
+    //   clearTimeout(timeoutRef.current);
+    // }
+    // setIsHovered(true);
+    timerRef.current = setTimeout(() => {
+      setIsHovered(true);
+    }, 100);
   };
 
   const handleMouseLeave = () => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
     timeoutRef.current = setTimeout(() => {
       console.log("a");
       setIsHovered(false);
     }, 50); // 50ミリ秒のディレイを設定
   };
 
+  // useEffect(() => {
+  //   return () => {
+  //     if (timeoutRef.current) {
+  //       clearTimeout(timeoutRef.current);
+  //     }
+  //   };
+  // }, []);
   useEffect(() => {
     return () => {
-      if (timeoutRef.current) {
-        clearTimeout(timeoutRef.current);
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
       }
     };
   }, []);
-
+  
   let StampComponent: ()=>JSX.Element;
   if (timeValue == 0) {
     StampComponent = (() => (<NilStamp />))
@@ -51,7 +65,7 @@ const Stamp = (props:{
 
 
   return (
-    <div onMouseEnter={handleMouseLeave} className='pr-1'>
+    <div className='pr-1'>
       <div
         ref={containerRef}
         className="relative inline-block"
@@ -59,14 +73,14 @@ const Stamp = (props:{
         onMouseLeave={handleMouseLeave}
       >
         
-        <div onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)}>
+        <div>
           <StampComponent />
         </div>
 
         {isHovered ? 
           <div
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave} 
+            // onMouseEnter={handleMouseEnter}
+            // onMouseLeave={handleMouseLeave} 
             className='absolute z-10 w-32 p-2 -mt-5 -mr-5 translate-x-2 bg-gray-800 rounded-lg shadow-md border border-gray-200'>
             <div className="text-gray-200"><p>03:45:59</p></div>
           </div>
