dfc73cc6de708ffb145b3fd444bb255d5643cd5a
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-02-04 23:06:11 +0900

    commit stats 取得

--- src-tauri/src/database/github.rs
+++ src-tauri/src/database/github.rs
@@ -1,7 +1,7 @@
 use anyhow::{Ok, Result};
 use chrono::{DateTime, Utc};
 use futures::TryStreamExt;
-use reqwest::{self, Client};
+use reqwest::{self, header::ETAG, Client};
 use serde::{Deserialize};
 use sqlx::{SqlitePool, Row};
 
@@ -11,32 +11,43 @@ type DbResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 #[derive(Deserialize, Debug)]
 struct ApiResps {
-    sha: String,
-    commit: CommitInfo
+	sha: String,
+	commit: CommitInfo
 }
 #[derive(Deserialize, Debug)]
 struct CommitInfo {
-    author: AuthorInfo
+	committer: AuthorInfo
 }
 #[derive(Deserialize, Debug)]
 struct AuthorInfo {
-    date: String
+	name: String,
+  date: String
 }
 #[derive(Deserialize, Debug)]
 pub struct CommitData {
-    sha: String,
-    date: String
+	sha: String,
+	date: String
 }
 #[derive(Deserialize, Debug)]
 pub struct BranchInfo {
-    name: String,
+	name: String,
 }
 #[derive(Debug)]
 pub struct Branch {
-    id: i64,
-    project_id: i64,
-    branch_name: String,
-    is_working: bool
+	id: i64,
+	project_id: i64,
+	branch_name: String,
+	is_working: bool
+}
+#[derive(Deserialize, Debug)]
+pub struct CommitCodeDiff {
+	total: i64,
+	additions: i64,
+	deletions: i64
+}
+#[derive(Deserialize, Debug)]
+pub struct CommitStats {
+	stats: CommitCodeDiff
 }
 
 pub(crate) async fn check_code_changes(pool: &SqlitePool, task_id: i64) -> Result<()> {
@@ -45,11 +56,15 @@ pub(crate) async fn check_code_changes(pool: &SqlitePool, task_id: i64) -> Resul
     println!("url {}",project.rep_url);
     let parts: Vec<&str> = project.rep_url.split('/').collect();
     if let (Some(owner), Some(repo)) = (parts.get(3), parts.get(4)) {
-      let prev_date = "2023-12-21T11:43:27Z".to_string();
+      let prev_date = "2023-02-02T11:43:27Z".to_string();
       let branches = get_activ_branch(owner.to_string(), repo.to_string(), pool, project.id).await?;
+			if branches.len() == 1 && branches[0].name == "main" {
+
+			}
 			// println!("b: {:?}",branches);
 			for b in branches {
-				get_commit_info(owner.to_string(), repo.to_string(), b.name, prev_date.clone()).await?
+				let commit_stats = get_commit_info(owner.to_string(), repo.to_string(), b.name.clone(), prev_date.clone()).await?;
+				println!("{}: {:?}", b.name, commit_stats);
 			}
     } else {
       println!("GitHubのユーザーとリポジトリが確認できませんでした");
@@ -99,8 +114,7 @@ pub(crate) async fn get_branches(pool: &SqlitePool, project_id: i64) -> Result<V
 	Ok(branches)
 } 
 
-pub(crate) async fn get_commit_info(owner:String, repo:String, branch: String, prev_date:String) -> Result<()> {
-	println!("\nok\n");
+pub(crate) async fn get_commit_info(owner:String, repo:String, branch: String, prev_date:String) -> Result<CommitCodeDiff> {
 	let client = Client::new();
 	let previous_date: DateTime<Utc> = prev_date.parse().expect("Invalid date format");
 	let url = format!("https://api.github.com/repos/{}/{}/commits", owner, repo);
@@ -117,10 +131,26 @@ pub(crate) async fn get_commit_info(owner:String, repo:String, branch: String, p
 	println!("取得完了");
 	let commit_datas: Vec<CommitData> = api_resps
 		.iter()
-		.filter(|res| previous_date < res.commit.author.date.clone().parse::<DateTime<Utc>>().expect("Invalid date format"))
-		.map(|res| CommitData{sha: res.sha.clone(), date: res.commit.author.date.clone()})
+		.filter(|res| res.commit.committer.date.clone().parse::<DateTime<Utc>>().expect("Invalid date format") > previous_date )
+		.filter(|res| res.commit.committer.name == "GitHub" )
+		.map(|res| CommitData{sha: res.sha.clone(), date: res.commit.committer.date.clone()})
 		.collect();
-	println!("コミット情報 {:?}", commit_datas);
+	// println!("コミット情報 {:?}", commit_datas);
 	// println!("おしまい");
-	Ok(())
+	let mut return_stats = CommitCodeDiff{total: 0, additions: 0, deletions: 0};
+	for c in commit_datas {
+		let url = format!("https://api.github.com/repos/{}/{}/commits/{}", owner, repo, c.sha);
+		let commit_response = client
+			.get(url)
+			.header(reqwest::header::USER_AGENT, "my-tauri-app")
+			.send()
+			.await?;
+		let commit_stats: CommitStats = commit_response.json().await?;
+		return_stats.total += commit_stats.stats.total;
+		return_stats.additions += commit_stats.stats.additions;
+		return_stats.deletions += commit_stats.stats.deletions;
+	}
+	Ok(return_stats)
 }
+
+
