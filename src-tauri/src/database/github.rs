d15d1ffbf239cc07712cb809c905d14ea68f5963
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-02-05 23:26:16 +0900

    get last date

--- src-tauri/src/database/github.rs
+++ src-tauri/src/database/github.rs
@@ -23,7 +23,7 @@ struct AuthorInfo {
 	name: String,
   date: String
 }
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, PartialEq, PartialOrd)]
 pub struct CommitData {
 	sha: String,
 	date: String
@@ -47,25 +47,39 @@ pub struct CommitCodeDiff {
 }
 #[derive(Deserialize, Debug)]
 pub struct CommitStats {
-	stats: CommitCodeDiff
+	stats: CommitCodeDiff,
+	commit: CommitInfo
 }
 
-pub(crate) async fn check_code_changes(pool: &SqlitePool, task_id: i64) -> Result<()> {
+pub(crate) async fn check_code_changes(pool: &SqlitePool, task_id: i64) -> Result<CommitCodeDiff> {
   let projects = project::get_project_belongs_task(pool, task_id).await?;
-  for project in &projects {
+	let mut total_code_chanses = CommitCodeDiff{total: 0, additions: 0, deletions: 0};
+  let mut last_date: String;
+	for project in &projects {
     println!("url {}",project.rep_url);
     let parts: Vec<&str> = project.rep_url.split('/').collect();
     if let (Some(owner), Some(repo)) = (parts.get(3), parts.get(4)) {
+			// *** projectテーブルから取得
       let prev_date = "2023-02-02T11:43:27Z".to_string();
       let branches = get_activ_branch(owner.to_string(), repo.to_string(), pool, project.id).await?;
 			if branches.len() == 1 && branches[0].name == "main" {
 
 			}
 			// println!("b: {:?}",branches);
+			let mut total_code_chanses = CommitCodeDiff{total: 0, additions: 0, deletions: 0};
 			for b in branches {
-				let commit_stats = get_commit_info(owner.to_string(), repo.to_string(), b.name.clone(), prev_date.clone()).await?;
+				let (commit_stats, branch_last_date) = get_commit_info(owner.to_string(), repo.to_string(), b.name.clone(), prev_date.clone()).await?;
 				println!("{}: {:?}", b.name, commit_stats);
+				total_code_chanses.total += commit_stats.total;
+				total_code_chanses.additions += commit_stats.additions;
+				total_code_chanses.deletions += commit_stats.deletions;
+				let utc_last: DateTime<Utc> = last_date.parse().expect("Invalid date format");
+				let utc_branch: DateTime<Utc> = branch_last_date.parse().expect("Invalid date format");
+				if utc_last < utc_branch {
+					last_date = branch_last_date;
+				}
 			}
+			// *** projectテーブルのデータ更新
     } else {
       println!("GitHubのユーザーとリポジトリが確認できませんでした");
     }
@@ -74,7 +88,7 @@ pub(crate) async fn check_code_changes(pool: &SqlitePool, task_id: i64) -> Resul
   // let project_branchs = project::get_project_with_branches(pool, 0).await?;
   // println!("プロジェクトブランチ {:?}", project_branchs.branches);
   // github::hello().await?;
-	Ok(())
+	Ok(total_code_chanses)
 }
 
 pub(crate) async fn get_activ_branch(owner:String, repo:String,pool: &SqlitePool, project_id: i64) -> Result<Vec<BranchInfo>> {
@@ -114,7 +128,7 @@ pub(crate) async fn get_branches(pool: &SqlitePool, project_id: i64) -> Result<V
 	Ok(branches)
 } 
 
-pub(crate) async fn get_commit_info(owner:String, repo:String, branch: String, prev_date:String) -> Result<CommitCodeDiff> {
+pub(crate) async fn get_commit_info(owner:String, repo:String, branch: String, prev_date:String) -> Result<(CommitCodeDiff, String)> {
 	let client = Client::new();
 	let previous_date: DateTime<Utc> = prev_date.parse().expect("Invalid date format");
 	let url = format!("https://api.github.com/repos/{}/{}/commits", owner, repo);
@@ -138,6 +152,7 @@ pub(crate) async fn get_commit_info(owner:String, repo:String, branch: String, p
 	// println!("コミット情報 {:?}", commit_datas);
 	// println!("おしまい");
 	let mut return_stats = CommitCodeDiff{total: 0, additions: 0, deletions: 0};
+	let mut last_date = prev_date;
 	for c in commit_datas {
 		let url = format!("https://api.github.com/repos/{}/{}/commits/{}", owner, repo, c.sha);
 		let commit_response = client
@@ -149,8 +164,13 @@ pub(crate) async fn get_commit_info(owner:String, repo:String, branch: String, p
 		return_stats.total += commit_stats.stats.total;
 		return_stats.additions += commit_stats.stats.additions;
 		return_stats.deletions += commit_stats.stats.deletions;
+		let change_date: DateTime<Utc> = commit_stats.commit.committer.date.parse().expect("Invalid date format");
+		let las_date: DateTime<Utc> = last_date.parse().expect("Invalid date format");
+		if change_date > las_date {
+			last_date = commit_stats.commit.committer.date;
+		}
 	}
-	Ok(return_stats)
+	Ok((return_stats, last_date))
 }
 
 
